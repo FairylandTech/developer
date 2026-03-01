90b8ccea721e79b426862a6a5d95f73bcfc2aad9
Lionel Johnson <fairylandfuture@protonmail.com>
2026-03-01 10:41:09 +0800

    ✨ feat(html): add multiple HTML files for JavaScript examples

---
+++ workspace/javascript/origin/simple/assets/js/quote.js
@@ -0,0 +1,9 @@
+/****************************************************
+ * @software: WebStorm
+ * @author: Lionel Johnson
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @datetime: 2026-03-01 08:14:56 UTC+08:00
+ ***************************************************/
+ 
+alert("Hello, quote.js loaded.");
