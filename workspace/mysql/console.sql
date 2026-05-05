6c1e9dcfc2f600b0f75ca88f70cd05033beb39e9
Beau Dean <fairylandfuture@outlook.com>
2026-05-06 01:47:32 +0800

    ✨ feat(toolkit): add Conda and Pip configuration files for optimized environment setup
    - Added `.condarc` for Conda configuration with optimized performance, network, and storage settings.
    - Added `pip.ini` for Pip configuration with improved network prioritization and reduced disk usage.
    - Unified Conda and Pip sources to prioritize domestic mirrors with fallback to official repositories.
    - Enhanced configurations to reduce environment setup time and prevent disk space issues.
    
    Closes #123

---
+++ workspace/mysql/console.sql
@@ -0,0 +1,24 @@
+/*****************************************************
+ * @software: IntelliJ IDEA
+ * @author: Beau Dean
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @datetime: 2026-05-05 00:15:42 UTC+08:00
+ *****************************************************/
+
+create database if not exists dev character set utf8mb4 collate utf8mb4_general_ci;
+use dev;
+drop table if exists t_user;
+create table if not exists t_user
+(
+    id         bigint auto_increment primary key comment 'id',
+    username   varchar(15)  not null comment '用户名',
+    password   varchar(255) not null comment '密码',
+    phone      varchar(11)  not null comment '手机号',
+    info       text comment '用户信息',
+    status     ENUM ('Y', 'N') default 'Y' comment '状态',
+    balance    decimal(10, 2)  default 0.00 comment '余额',
+    created_at timestamp       default current_timestamp comment '创建时间',
+    updated_at timestamp       default current_timestamp on update current_timestamp comment '更新时间',
+    enabled    ENUM ('Y', 'N') default 'Y' comment '是否启用'
+) comment '用户表';
--- workspace/postgresql/console.sql
+++ workspace/postgresql/console.sql
@@ -12,3 +12,19 @@ grant all privileges on database dev to dev;
 
 create extension vector;
 SELECT * FROM pg_extension where extname = 'vector';
+
+create schema if not exists t_dev authorization dev;
+
+set search_path to t_dev;
+create table if not exists t_dev.t_user(
+    id serial primary key,
+    username varchar(15) not null,
+    password varchar(255) not null,
+    phone varchar(11) not null,
+    info text,
+    status ENUM('Y', 'N') default 'Y',
+    balance decimal(10, 2) default 0.00,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    enabled ENUM('Y', 'N') default 'Y'
+)
